681ca95180207d05f57734bdda9fc63794f21d54
PhenomK <Ghope.plus@gmail.com>
2017-09-28 14:15:10 +0800

    Signed-off-by: PhenomK <Ghope.plus@gmail.com>

--- Requests+Unittest/user create.py
+++ Requests+Unittest/user create.py
@@ -9,7 +9,7 @@ class Create_User(unittest.TestCase):
         querystring = {"user_slug":"admin","password":"123456"}
         response1 = requests.request("post", url, params=querystring)
         response2 = requests.request("post", url, params=querystring).json()
-        print(response2)
+        #print(response2)
         line0 = str(response1.cookies)
         line1 = str(response2)                              # body
         var1 = str(re.findall(r"'uid': (.+?),", line1))
@@ -24,10 +24,29 @@ class Create_User(unittest.TestCase):
         line6 = str(re.sub(r"\W", "", var5))              # JSESSIONID
         i = str(random.randint(0, 1000))
         url2 = "http://172.16.29.37:8080/v2/user/create"
-        querystring2 = {"path_type": "self", "is_update": "false", "account_id": line3, "uid": line2, "S": line4,
-                        "X-LENOVO-SESS-ID": line5, "JSESSIONID": line6}
-        response = requests.request("POST", url2, params=querystring2).json()
-        # print(response)
+        querystring2 = {"user_name": "lenovo"+i,        #用户名
+                        "user_slug": "lenovo"+i,        #登录名
+                        "password":"123456",            #密码
+                        "email":"lenovo@lenovocloud.com"+i,         #邮箱
+                        "quota":"1073741824",           #本地空间大小1G
+                        "use_local_quota":"1",          #开启本地空间
+                        "use_cloud_quota":"1",          #开启云端空间
+                        "cloud_quota":"1073741824",    #云端空间大小1G
+                        "cloud_allow_scan":"1",         #开启企业空间
+                        "use_threshold":"0",            #不限速
+                        "active":"true",                 #不冻结用户
+                        "account_id": line3,
+                        "uid": line2,
+                        "S": line4,
+                        "X-LENOVO-SESS-ID": line5,
+                        "JSESSIONID": line6}
+        response3 = requests.request("POST", url2, params=querystring2).json()
+        #print(response3)
+        self.assertRegexpMatches(response3['user_slug'], 'lenovo'+ i )
+        self.assertRegexpMatches(response3['user_name'], 'lenovo'+ i )
+        self.assertRegexpMatches(response3['email'], 'lenovo@lenovocloud.com' + i )
+if __name__ == '__main__':
+    unittest.main()
 
 
 
